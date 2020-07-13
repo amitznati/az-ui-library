56117e1c92dc5ffc59ce9036f28b3eb1c6d0d0c2
amitznati-work-hp <amitznati@gmail.com>
2020-07-13 20:33:38 +0300

    init create-react-library@3.1.1

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'az-ui-library'
+import 'az-ui-library/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
