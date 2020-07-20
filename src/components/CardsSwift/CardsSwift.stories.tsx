27124d1c79732fa5933531becee2b3b11f74c8d1
amitznati-work-hp <amitznati@gmail.com>
2020-07-21 01:13:56 +0300

    Update CardsSwift.stories.tsx

--- src/components/CardsSwift/CardsSwift.stories.tsx
+++ src/components/CardsSwift/CardsSwift.stories.tsx
@@ -19,7 +19,6 @@ export const CardsSwiftDemo = () => {
     return (
       <div
         style={{
-          margin: 'auto',
           boxShadow: '0 1px 4px black',
           height: '100%',
           backgroundColor: 'orangered',
@@ -33,19 +32,25 @@ export const CardsSwiftDemo = () => {
       </div>
     );
   };
+  const moveUp = (): void => {
+    setActiveIndex(activeIndex + 1);
+  };
+  const moveDown = (): void => {
+    setActiveIndex(activeIndex - 1);
+  };
   return (
     <div>
       <CardsSwift
         data={data}
         renderItem={renderItem}
         activeIndex={activeIndex}
-        onSwiftLeft={() => setActiveIndex(activeIndex + 1)}
-        onSwiftRight={() => setActiveIndex(activeIndex - 1)}
+        onSwiftLeft={moveUp}
+        onSwiftRight={moveDown}
         height={400}
       />
       <div className="cards-swift-stories-buttons">
-        <span onClick={() => setActiveIndex(activeIndex + 1)}>+</span>
-        <span onClick={() => setActiveIndex(activeIndex - 1)}>-</span>
+        <span onClick={moveUp}>+</span>
+        <span onClick={moveDown}>-</span>
       </div>
     </div>
   );
