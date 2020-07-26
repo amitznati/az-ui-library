1b607bffcc7e93a76fd8ab77bd30c03755634ca9
amitznati-work-hp <amitznati@gmail.com>
2020-07-27 00:40:39 +0300

    Update IconButton.stories.tsx

--- src/components/IconButton/IconButton.stories.tsx
+++ src/components/IconButton/IconButton.stories.tsx
@@ -3,7 +3,7 @@ import { color, number, withKnobs } from '@storybook/addon-knobs';
 import IconButton from './IconButton';
 import iconPack from '../../styles/assets/icons/sprite.svg';
 import { ReactComponent as BellIcon } from '../../styles/assets/icons/bell.svg';
-import { ReactComponent as ArrowIcon } from '../../styles/assets/icons/arrow_forward_ios-24px.svg'
+import { ReactComponent as ArrowIcon } from '../../styles/assets/icons/arrow_forward_ios-24px.svg';
 // @ts-ignore
 import stylesVars from '../../styles/abstracts/_variables.scss';
 
@@ -18,31 +18,31 @@ export default {
   ]
 };
 console.log(BellIcon);
+const groupAppearance = 'IconButton-Appearance';
+const groupColors = 'IconButton-Colors';
+const sizeValue = number('Size', 50, {}, groupAppearance);
+
+const backgroundColorValue = color(
+  'backgroundColor',
+  'transparent',
+  groupColors
+);
+const backgroundColorHoverValue = color(
+  'backgroundColorHover',
+  stylesVars.whiteColor,
+  groupColors
+);
+const iconColorValue = color('iconColor', stylesVars.whiteColor, groupColors);
+const iconColorHoverValue = color(
+  'iconColorHover',
+  stylesVars.primaryColor,
+  groupColors
+);
 export const Icons = () => {
-  const groupAppearance = 'IconButton-Appearance';
-  const groupColors = 'IconButton-Colors';
-  const sizeValue = number('Size', 50, {}, groupAppearance);
-
-  const backgroundColorValue = color(
-    'backgroundColor',
-    'transparent',
-    groupColors
-  );
-  const backgroundColorHoverValue = color(
-    'backgroundColorHover',
-    stylesVars.whiteColor,
-    groupColors
-  );
-  const iconColorValue = color('iconColor', stylesVars.whiteColor, groupColors);
-  const iconColorHoverValue = color(
-    'iconColorHover',
-    stylesVars.primaryColor,
-    groupColors
-  );
-
   return (
     <div className="buttons-stories">
       <div>
+        {/* import iconPack from '../../styles/assets/icons/sprite.svg'; */}
         <IconButton
           size={60}
           iconColor="orangered"
@@ -50,6 +50,7 @@ export const Icons = () => {
         />
       </div>
       <div>
+        {/* import { ReactComponent as ArrowIcon } from '../../styles/assets/icons/arrow_forward_ios-24px.svg'; */}
         <IconButton
           size={sizeValue}
           iconColor={iconColorValue}
